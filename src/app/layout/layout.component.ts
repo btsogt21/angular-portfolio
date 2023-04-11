761c9a8e938a3fab3264a05bf3727a328a847e94
btsogt21 <manlaitsoogii@gmail.com>
2023-04-11 01:42:45 -0500

    implemented hover effect to open and close the sidebar using a chevron icon that switches between pointing left and right depending on if the sidebar is collapsed or not.  Made sure functionality and icon were integrated properly with other functionalities like dark mode and light mode. Added a little GIF as well.

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-layout',
@@ -9,12 +11,20 @@ import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 export class LayoutComponent {
   isCollapsed = false;
   isDarkMode = false;
-  toggleSidebar(){
-    this.isCollapsed = !this.isCollapsed;
+  openSidebar(){
+    this.isCollapsed = false;
   }
+  closeSidebar(){
+    this.isCollapsed = true;
+  }
+  // toggleSidebar(){
+  //   this.isCollapsed = !this.isCollapsed;
+  // }
   toggleDarkMode(){
     this.isDarkMode = !this.isDarkMode;
   }
   mySun = faSun;
   myMoon = faMoon;
+  myChevronRight = faChevronRight;
+  myChevronLeft = faChevronLeft;
 }
