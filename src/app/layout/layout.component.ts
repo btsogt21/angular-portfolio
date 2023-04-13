f710c421de40d802d7b919ac394aa5bc6f426fed
btsogt21 <manlaitsoogii@gmail.com>
2023-04-13 01:04:23 -0500

    Moving zaheer gif to be after the rest of the layout, center justified. Adjusted navbar to fill the rest of the space left behind by the gif and some. Tried to add some wind animation to accompany the opening and collapsing of the sidebar, but having trouble with the styling at the moment.

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,21 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef} from '@angular/core';
 import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
-
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent {
-  isCollapsed = true;
+  isCollapsed = false;
   isDarkMode = false;
+  @ViewChild('windContainer') windContainer?: ElementRef;
   openSidebar(){
     this.isCollapsed = false;
+    this.showWindEffect(true);
   }
   closeSidebar(){
     this.isCollapsed = true;
+    this.showWindEffect(false);
   }
   // toggleSidebar(){
   //   this.isCollapsed = !this.isCollapsed;
@@ -23,8 +25,15 @@ export class LayoutComponent {
   toggleDarkMode(){
     this.isDarkMode = !this.isDarkMode;
   }
+  showWindEffect(visible: boolean){
+    if(this.windContainer && this.windContainer.nativeElement){
+      this.windContainer.nativeElement.style.display = visible ? 'block' : 'none';
+    }
+  }
+
   mySun = faSun;
   myMoon = faMoon;
   myChevronRight = faChevronRight;
   myChevronLeft = faChevronLeft;
 }
+
