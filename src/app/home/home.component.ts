842dc2864fd1537afc4702ee159397cac027ed83
btsogt21 <manlaitsoogii@gmail.com>
2023-06-24 02:02:13 -0400

    beginning of adding dark-mode to body component, ironing out minor issues

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ThemeService } from '../theme.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  // isDark = false;
+  
+  // private darkModeSubscription: Subscription;
+  // constructor(private themeService: ThemeService) { 
+  //   this.darkModeSubscription = this.themeService.isDarkMode$.subscribe(isDarkMode => {
+  //     this.isDark = isDarkMode;
+  //   });
+  // }
+  // toggleDarkMode(): void{
+  //   this.themeService.toggleDarkMode();
+  // }
+  // ngOnInit(): void {
+
+  // }
+
+  // ngOnDestroy(): void {
+  //   this.darkModeSubscription.unsubscribe();
+  // }
 
 }
